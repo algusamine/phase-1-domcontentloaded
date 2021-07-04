119d7266f20c7b39b2e7bf5204ece99a58888d6a
Amine <algusamine@gmail.com>
2021-07-04 01:15:15 -0400

    commit tree 5003aa5fbcda697df79e10f0c51c60d30b40de3e

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const text = document.querySelector('p');
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    return e? text.textContent = "This is really cool!": "JavaScript is so cool. It lets me add text to my page programmatically." ;
+  });
+  console.log("The DOM has loaded");
